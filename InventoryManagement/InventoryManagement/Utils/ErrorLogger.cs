6994be1d8c00284bfe789f84f74846dbd9fcab15
mapuaninja <mapuaninja@gmail.com>
2017-01-11 11:04:27 +0800

    latest

---
+++ InventoryManagement/InventoryManagement/Utils/ErrorLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Utils
+{
+    class ErrorLogger
+    {
+        public static void LogException(Exception ex)
+        {
+            var log = (DateTime.Now.ToString());
+            log += Environment.NewLine;
+            log += (ex.Message);
+            log += Environment.NewLine;
+            log += (ex.StackTrace);
+
+            Write(log);
+        }
+        private static void Write(string log)
+        {
+            
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "error.txt");
+            if (!File.Exists(path))
+            {
+               var fs =  File.Create(path); 
+                fs.Close();
+                fs.Dispose();
+            }
+
+
+
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(path, true))
+            {
+                file.WriteLine(log);
+            }
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement/Utils/LDAPAuth.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace InventoryManagement.Utils
+{
+    public class LdapAuth
+    {
+        public static bool IsInRole(string domain, string username, string password, string role)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, domain, username, password))
+            {
+                try
+                {
+                    GroupPrincipal group = GroupPrincipal.FindByIdentity(context, IdentityType.SamAccountName, role);
+                    UserPrincipal user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username);
+                    return IsInGroup(user, group);
+                }
+                catch (Exception ex)
+                {
+                    ErrorLogger.LogException(ex);
+                    return false;
+                }
+                
+            }
+        }
+
+        public static bool IsInGroup(Principal principal, GroupPrincipal group)
+        {
+            if (principal.IsMemberOf(group))
+                return true;
+
+            foreach (var g in principal.GetGroups())
+            {
+                if (IsInGroup(g, group))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
